c6ca3430b10a7fbdfe72fc10175aaa0fb67cffb4
Ian Lin <ianrp.lin@gmail.com>
2023-05-02 14:37:12 -0400

    Add insert transaction functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 load_dotenv()
 
@@ -14,3 +14,35 @@ def get_db_connection():
                             user=os.environ['DB_USERNAME'],
                             password=os.environ['DB_PASSWORD'])
     return conn
+
+
+# Add a transaction
+@app.route("/add", methods=['POST'])
+def add_transaction():
+
+    data = request.get_json()
+
+    # Get connection and create curosor
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Insert transaction data into database
+    cur.execute('INSERT INTO transactions (date, vendor, category, amount, notes)'
+                'VALUES (%s, %s, %s, %s, %s)',
+                (data['date'],
+                 data['vendor'],
+                 data['category'],
+                 data['amount'],
+                 data['notes']))
+    conn.commit()
+
+    # Close cursor and connection with database
+    cur.close()
+    conn.close()
+
+    return 'Transaction recorded', 200
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
