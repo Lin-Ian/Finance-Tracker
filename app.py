c1d16020a0e056c9d40babd021afa7411e5935b9
Ian Lin <ianrp.lin@gmail.com>
2023-05-12 11:16:15 -0400

    Edit connection parameters to prepare for cloud hosting

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='finance_tracker',
+    conn = psycopg2.connect(host=os.environ['DB_HOST'],
+                            database=os.environ['DB_NAME'],
                             user=os.environ['DB_USERNAME'],
                             password=os.environ['DB_PASSWORD'])
     return conn
