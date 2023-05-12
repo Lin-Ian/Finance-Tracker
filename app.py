3ce4a5e0196c9fbf14f79b444112d694f0114f10
Ian Lin <ianrp.lin@gmail.com>
2023-05-12 10:51:49 -0400

    Undo last commit

--- app.py
+++ app.py
@@ -278,9 +278,6 @@ def upload_transactions():
         # Remove file after contents uploaded to database
         os.remove(filename)
 
-    else:
-        message = "Please attach a file to upload"
-
     return render_template("upload.html", message=message, filename=filename)
 
 
